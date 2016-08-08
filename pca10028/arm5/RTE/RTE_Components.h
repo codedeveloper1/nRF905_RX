625ddc2c70eed8b33ce4d840dfce49a8c5fd4a7d
codedeveloper1 <fastcodedeveloper@gmail.com>
2016-08-08 14:43:01 +0530

    Add files via upload

--- pca10028/arm5/RTE/RTE_Components.h
+++ pca10028/arm5/RTE/RTE_Components.h
@@ -1,1 +1,26 @@
 
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'nRF905_RX' 
+ * Target:  'nrf51422_xxac' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+#define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define GPIOTE_ENABLED
+  #define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define NRF_LOG_USES_UART=1
+#define SWI_DISABLE0
+#define UART0_ENABLED
+
+#endif /* RTE_COMPONENTS_H */
